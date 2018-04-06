0b942682e6a2f751f0d3682757d5d88275854506
maarten peels <maartenpeels@gmail.com>
2018-04-06 13:10:36 +0200

    Execute Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserTypesSeeder::class);
+        $this->call(WorkingHoursSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
