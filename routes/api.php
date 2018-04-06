10f1a52f2aa1353e85360b732a53ea6b58d1e700
maarten peels <maartenpeels@gmail.com>
2018-04-06 13:12:50 +0200

    Setup routes(login)

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('login', 'UserController@attemptLogin');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    Route::group(['middleware' => ['auth:api', 'throttle:60,1']], function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        //Login protected routes
+
+    });
 });
